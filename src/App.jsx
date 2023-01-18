82b815fd7ba11a1b30f350062d673d6c92992a9f
Chadd Ho <74035551+YogaRecodingDNA@users.noreply.github.com>
2023-01-17 17:42:21 -0800

    DROPDOWN: Dropdown page displaying prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { useState } from 'react';
-import Dropdown from "./components/Dropdown";
-
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  }
-
-  const options = [
-    { label: 'Yama', value: 'yama' },
-    { label: 'Niyama', value: 'niyama' },
-    { label: 'Asana', value: 'asana' },
-    { label: 'Pranayama', value: 'pranayama' }
-  ];
 
   return (
-    <div className="flex">
-      <Dropdown value={selection} onChange={handleSelect} options={options} />
-      <Dropdown value={selection} onChange={handleSelect} options={options} />
+    <div>
+      App
     </div>
   );
 }
---
+++ src/pages/DropdownPage.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Dropdown from "./components/Dropdown";
+
+const DropdownPage = () => {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  }
+
+  const options = [
+    { label: 'Yama', value: 'yama' },
+    { label: 'Niyama', value: 'niyama' },
+    { label: 'Asana', value: 'asana' },
+    { label: 'Pranayama', value: 'pranayama' }
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown value={selection} onChange={handleSelect} options={options} />
+    </div>
+  );
+}
+
+export default DropdownPage;
