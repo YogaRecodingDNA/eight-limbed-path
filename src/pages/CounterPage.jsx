ae6f6d7e6e044ff5bc4e22ba09537a9f60b70985
Chadd Ho <74035551+YogaRecodingDNA@users.noreply.github.com>
2023-01-20 14:18:22 -0800

    USE_REDUCER: Implement Increment/Decrement/Addiition functionality with useReducer

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,18 +1,115 @@
+import { useReducer } from 'react';
+import Panel from '../components/Panel';
 import Button from '../components/Button';
-import useUsefulHook from '../hooks/use-usefulHook';
+
+// REDUCER ACTION TYPES
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'deccrement';
+const SET_VALUE_TO_ADD = 'change-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT_COUNT:
+      return { ...state, count: state.count - 1 };
+    case SET_VALUE_TO_ADD:
+      return { ...state, valueToAdd: action.payload };
+    case ADD_VALUE_TO_COUNT:
+      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+    default:
+      return state;
+  }
+};
 
 const CounterPage = ({ initialCount }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0
+  });
 
-  const { count, handleClick } = useUsefulHook(initialCount);
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT
+    });
+  }
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT
+    });
+  }
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: ADD_VALUE_TO_COUNT
+    });
+   }
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={handleClick}>
-        Increment
-      </Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Add to Count</label>
+        <input
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   )
 }
 
 export default CounterPage;
+
+
+
+
+/** IMMER VERSION ==== IMMER VERSION ===== IMMER VERSION ===== IMMER VERSION =================
+ * 
+ import produce from 'immer';
+
+ const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.setValueToAdd = action.payload;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.setValueToAdd = 0;
+      return;
+    default:
+  }
+};
+ 
+  Only change to useReducer is adding produce() wrapper
+ const [state, dispatch] = useReducer(produce(reducer), {
+    count: initialCount,
+    valueToAdd: 0
+  });
+ */
